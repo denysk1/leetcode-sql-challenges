4d4d3dec1df1f407ced78613105de367d2430690
Denys K <35410460+denys1@users.noreply.github.com>
2025-05-13 18:20:06 +0300

    add 610

---
+++ easy/610. Triangle Judgement.sql
@@ -0,0 +1,54 @@
+/*
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+
+
+-- Solution:
+select 
+    t.x,
+    t.y,
+    t.z,
+    case
+        when t.x + t.y > t.z
+            and t.y + t.z > t.x
+            and t.z + t.x > t.y then 'Yes'
+        else 'No'
+    end as 'triangle'
+from Triangle as t
