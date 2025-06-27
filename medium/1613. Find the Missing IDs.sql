072759d20cdd12a668304e780725ca82025b797e
Denys K <35410460+denys1@users.noreply.github.com>
2025-06-27 12:35:13 +0300

    add 1613

---
+++ medium/1613. Find the Missing IDs.sql
@@ -0,0 +1,58 @@
+/*
+Table: Customers
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| customer_id   | int     |
+| customer_name | varchar |
++---------------+---------+
+customer_id is the column with unique values for this table.
+Each row of this table contains the name and the id customer.
+ 
+
+Write a solution to find the missing customer IDs. The missing IDs are ones that are not in the Customers table but are in the range between 1 and the maximum customer_id present in the table.
+
+Notice that the maximum customer_id will not exceed 100.
+
+Return the result table ordered by ids in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++-------------+---------------+
+| customer_id | customer_name |
++-------------+---------------+
+| 1           | Alice         |
+| 4           | Bob           |
+| 5           | Charlie       |
++-------------+---------------+
+Output: 
++-----+
+| ids |
++-----+
+| 2   |
+| 3   |
++-----+
+Explanation: 
+The maximum customer_id present in the table is 5, so in the range [1,5], IDs 2 and 3 are missing from the table.
+*/
+
+-- Solution:
+with recursive cte as (
+    select 1 as expected_customer_id
+    union all
+    select expected_customer_id + 1
+    from cte
+    where expected_customer_id < (select max(customer_id) from Customers)
+)
+select
+    expected_customer_id as ids
+from Customers c
+right join cte on cte.expected_customer_id = c.customer_id 
+where c.customer_id is null;
